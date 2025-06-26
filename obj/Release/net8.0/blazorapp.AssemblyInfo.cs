b42fa18d64e0922944d176ed0b713e73938074af
haroldmd42 <yanmd4266@gmail.com>
2025-06-26 14:56:00 -0500

    Feat. mas updates

--- obj/Release/net8.0/blazorapp.AssemblyInfo.cs
+++ obj/Release/net8.0/blazorapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("blazorapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+709f7d44096d877296575326cf88d3dbb4cac09e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbd8b7171ab105a63052ab3d8d6ac7d737a747fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("blazorapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("blazorapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
